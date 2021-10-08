fdd8eadcd7125bd802009fe1eb340e5eb5269872
Leonardo Funez <leonardo.funez@ix.co>
2021-10-07 19:26:27 -0500

    Comment isMobile function

--- utils/is-mobile.js
+++ utils/is-mobile.js
@@ -1,10 +1,11 @@
 const isMobile = () => {
-    if (typeof window !== "undefined") {
-        return (
-            window.navigator.userAgent.toLowerCase().indexOf("mobile") !== -1
-        )
-    }
     return false
+    // if (typeof window !== "undefined") {
+    //     return (
+    //         window.navigator.userAgent.toLowerCase().indexOf("mobile") !== -1
+    //     )
+    // }
+    // return false
 }
 
 export default isMobile
